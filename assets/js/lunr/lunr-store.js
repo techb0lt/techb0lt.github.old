d0f0d7ae7953c276d5bed9f16730b79b46cd8ebc
techb0lt <techb0lt@users.noreply.github.com>
2023-03-09 20:45:19 +0000

    jekyll build from Action 40549ebc2da5a27409063753ee393261a9a2f6c0

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "Post: Modified Date",
+        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/blog/post-modified/",
+        "teaser": null
+      },{
+        "title": "Post: Standard",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/blog/post-standard/",
+        "teaser": null
+      },{
+        "title": "Post: Chat",
+        "excerpt":"Abbott: Strange as it may seem, they give ball players nowadays very peculiar names. Costello: Funny names? Abbott: Nicknames, nicknames. Now, on the St. Louis team we have Who’s on first, What’s on second, I Don’t Know is on third– Costello: That’s what I want to find out. I want...","categories": ["Blog"],
+        "tags": ["chat","Post Formats"],
+        "url": "/blog/post-chat/",
+        "teaser": null
+      },{
+        "title": "Post: Notice",
+        "excerpt":"A notice displays information that explains nearby content. Often used to call attention to a particular detail. When using Kramdown {: .notice} can be added after a sentence to assign the .notice to the &lt;p&gt;&lt;/p&gt; element. Changes in Service: We just updated our privacy policy here to better service our...","categories": ["Blog"],
+        "tags": ["Post Formats","notice"],
+        "url": "/blog/post-notice/",
+        "teaser": null
+      },{
+        "title": "Post: Quote",
+        "excerpt":"   Only one thing is impossible for God: To find any sense in any copyright law on the planet.       Mark Twain   ","categories": ["Blog"],
+        "tags": ["Post Formats","quote"],
+        "url": "/blog/post-quote/",
+        "teaser": null
+      },{
+        "title": "Post: Link",
+        "excerpt":"This theme supports link posts, made famous by John Gruber. To use, just add link: http://url-you-want-linked to the post’s YAML front matter and you’re done.      And this is how a quote looks.    Some link can also be shown.  ","categories": ["Blog"],
+        "tags": ["link","Post Formats"],
+        "url": "/blog/post-link/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"In PlantUML:      -&gt;: Arrow is used for delivery message   --&gt;: Dotted Arrow is used for return message   &lt;img class=\"plantuml\" src=\"http://www.plantuml.com/plantuml/svg/~h407374617274756d6c0a416c696365202d3e20426f623a2041757468656e7469636174696f6e20526571756573740a426f62202d2d3e20416c6963653a2041757468656e7469636174696f6e20526573706f6e73650a0a416c696365202d3e20426f623a20416e6f746865722061757468656e7469636174696f6e20526571756573740a416c696365203c2d2d20426f623a20416e6f746865722061757468656e7469636174696f6e20526573706f6e73650a40656e64756d6c\"&gt;     ","categories": ["blog"],
+        "tags": ["Processes","Jekyll","update"],
+        "url": "/blog/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Welcome to PUML!",
+        "excerpt":"In PlantUML:      -&gt;: Arrow is used for delivery message …   --&gt;: Dotted Arrow is used for return message                    ","categories": ["blog"],
+        "tags": ["Processes","Jekyll","update"],
+        "url": "/blog/welcome-to-puml/",
+        "teaser": null
+      }]
